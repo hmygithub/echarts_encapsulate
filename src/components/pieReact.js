ca35ebf0a13dbf73dc31fdef826e3d80893d7173
hmygithub <2713237776@qq.com>
2017-09-13 13:56:10 +0800

    组件componentDidMount时初始化echarts

--- src/components/pieReact.js
+++ src/components/pieReact.js
@@ -11,6 +11,50 @@ import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/legend'
 
 export class PieReact extends React.Component {
+    //在react中，我们需要等到虚拟dom渲染完成了才能通过refs去获取节点,所以在组件componentDidMount的时候初始化echarts
+    componentDidMount(){
+      const { data } = this.props //外部传入的data数据
+      let myChart = echarts.init(this.refs.pieChart) //初始化echarts
+
+      //我们要定义一个setPieOption函数将data传入option里面
+      let options = this.setPieOption(data)
+      //设置options
+      myChart.setOption(options)
+    }
+
+    //一个基本的echarts图表配置函数
+    setPieOption(data) {
+      return {
+        series:[
+          {
+            name: '负荷',
+            type: 'pie',
+            radius: ['100%', '70%'],
+            data: data, //传入外部的data数据
+            avoidLabelOverlap: false,
+            label: {
+              normal: {
+                show: false,
+                position: 'center'
+              },
+              emphasis: {
+                show: true,
+                textStyle: {
+                  fontSize: '30',
+                  fontWeight: 'bold'
+                }
+              }
+            },
+            labelLine:{
+              normal: {
+                show: false
+              }
+            }
+          }
+        ]
+      }
+    }
+
     render(){
         return(
           <div className="pie-react">
