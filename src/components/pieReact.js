63b625df459ad2dd9a9cf096feb082b0f77ec2a2
hmygithub <2713237776@qq.com>
2017-09-13 14:10:12 +0800

    使用componentDidUpdate监听props（data发生改变）的变化

--- src/components/pieReact.js
+++ src/components/pieReact.js
@@ -1,7 +1,7 @@
 /**
  * Created by lenovo on 2017/9/13.
  */
-import React, { Component } from 'react'
+import React, { Component,PropTypes } from 'react'
 
 //导入echarts
 var echarts = require('echarts/lib/echarts')
@@ -10,60 +10,69 @@ require('echarts/lib/component/title')
 import 'echarts/lib/component/tooltip'
 import 'echarts/lib/component/legend'
 
+//console.log(echarts);
 export class PieReact extends React.Component {
-    constructor(props) {
-      super(props)
-      this.setPieOption = this.setPieOption.bind(this)//绑定setPieOption到当前的实例。
-    }
-    //在react中，我们需要等到虚拟dom渲染完成了才能通过refs去获取节点,所以在组件componentDidMount的时候初始化echarts
-    componentDidMount(){
-      const { data } = this.props //外部传入的data数据
-      let myChart = echarts.init(this.refs.pieChart) //初始化echarts
+  constructor(props) {
+    super(props)
+    this.setPieOption = this.setPieOption.bind(this)//绑定setPieOption到当前的实例。
+  }
+  //在react中，我们需要等到虚拟dom渲染完成了才能通过refs去获取节点,所以在组件componentDidMount的时候初始化echarts
+  componentDidMount() {
+    const { data } = this.props //外部传入的data数据
+    let myChart = echarts.init(this.refs.pieChart) //初始化echarts
 
-      //我们要定义一个setPieOption函数将data传入option里面
-      let options = this.setPieOption(data)
-      //设置options
-      myChart.setOption(options)
-    }
+    //我们要定义一个setPieOption函数将data传入option里面
+    let options = this.setPieOption(data)
+    //设置options
+    myChart.setOption(options)
+  }
 
-    //一个基本的echarts图表配置函数
-    setPieOption(data) {
-      return {
-        series:[
-          {
-            name: '负荷',
-            type: 'pie',
-            radius: ['100%', '70%'],
-            data: data, //传入外部的data数据
-            avoidLabelOverlap: false,
-            label: {
-              normal: {
-                show: false,
-                position: 'center'
-              },
-              emphasis: {
-                show: true,
-                textStyle: {
-                  fontSize: '30',
-                  fontWeight: 'bold'
-                }
-              }
+  componentDidUpdate() {
+    const { data } = this.props;
+    let myChart = echarts.init(this.refs.pieChart)
+
+    let options = this.setPieOption(data)
+    myChart.setOption(options)
+  }
+
+  //一个基本的echarts图表配置函数
+  setPieOption(data) {
+    return {
+      series: [
+        {
+          name: '负荷',
+          type: 'pie',
+          radius: ['100%', '70%'],
+          data: data, //传入外部的data数据
+          avoidLabelOverlap: false,
+          label: {
+            normal: {
+              show: false,
+              position: 'center'
             },
-            labelLine:{
-              normal: {
-                show: false
+            emphasis: {
+              show: true,
+              textStyle: {
+                fontSize: '30',
+                fontWeight: 'bold'
               }
             }
+          },
+          labelLine: {
+            normal: {
+              show: false
+            }
           }
-        ]
-      }
+        }
+      ]
     }
+  }
 
-    render(){
-        return(
-          <div className="pie-react">
-            <div ref="pieReact" style={{width: "100%", height: '200px'}}></div>
-          </div>
-        )
-    }
+  render() {
+    return (
+      <div className="pie-react">
+        <div ref="pieReact" style={{width: "100%", height: '200px'}}></div>
+      </div>
+    )
+  }
 }
